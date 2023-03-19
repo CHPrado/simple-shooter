feef76de0877272cb3459ebfbb59229de16e99c3
Christian Prado <christianprado.contato@gmail.com>
2023-03-19 17:57:18 -0300

    Add Shoot, Heath and Death animation

--- Source/SimpleShooter/Gun.cpp
+++ Source/SimpleShooter/Gun.cpp
@@ -3,6 +3,9 @@
 
 #include "Gun.h"
 #include "Components/SkeletalMeshComponent.h"
+#include "Kismet/GameplayStatics.h"
+#include "DrawDebugHelpers.h"
+#include "Engine/DamageEvents.h"
 
 // Sets default values
 AGun::AGun()
@@ -19,7 +22,34 @@ AGun::AGun()
 
 void AGun::PullTrigger()
 {
-  UE_LOG(LogTemp, Display, TEXT("You've been shot!"));
+  UGameplayStatics::SpawnEmitterAttached(MuzzleFlash, Mesh, TEXT("MuzzleFlashSocket"));
+  APawn* OwnerPawn = Cast<APawn>(GetOwner());
+  if (OwnerPawn == nullptr) return;
+
+  AController* OwnerController = OwnerPawn->GetController();
+  if (OwnerController == nullptr) return;
+
+  FVector Location;
+  FRotator Rotation;
+  OwnerController->GetPlayerViewPoint(Location, Rotation);
+
+  FVector End = Location + Rotation.Vector() * MaxRange;
+
+  FHitResult Hit;
+  bool bSuccess = GetWorld()->LineTraceSingleByChannel(Hit, Location, End, ECC_GameTraceChannel1);
+
+  if (bSuccess)
+  {
+    FVector ShotDirection = -Rotation.Vector();
+    UGameplayStatics::SpawnEmitterAtLocation(GetWorld(), ImpactEffect, Hit.Location, ShotDirection.Rotation());
+    
+    AActor* HitActor = Hit.GetActor();
+    if (HitActor)
+    {
+      FPointDamageEvent DamageEvent(Damage, Hit, ShotDirection, nullptr);
+      HitActor->TakeDamage(Damage, DamageEvent, OwnerController, this);
+    }
+  }
 }
 
 // Called when the game starts or when spawned
